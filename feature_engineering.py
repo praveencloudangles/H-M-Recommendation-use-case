7e7cf9117630a1e6aa50babf03425b2b6b295cf0
praveencloudangles <112871831+praveencloudangles@users.noreply.github.com>
2024-03-20 18:22:15 +0530

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -6,7 +6,7 @@ import numpy as np
 def add_true_randomly(row):
 
     # Choose the number of columns to add True values randomly
-    num_columns = np.random.randint(3, 110)  # Choose randomly between 2 and 3 columns
+    num_columns = np.random.randint(3, 10)  # Choose randomly between 2 and 3 columns
 
     # Randomly select num_columns columns from the DataFrame
     chosen_columns = np.random.choice(df.columns, size=num_columns, replace=False)
@@ -24,6 +24,8 @@ df_modified = df.apply(add_true_randomly, axis=1)
 
 print(df_modified)
 df_modified.drop(columns=df_modified.columns[0], axis=1, inplace=True)
-print(df_modified)
 
-df_modified.to_csv("final.csv", index=False)
+df_updated=df_modified.iloc[:12]
+
+
+df_updated.to_csv("final.csv", index=False)
