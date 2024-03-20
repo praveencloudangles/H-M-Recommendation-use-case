115ea209547c116c753d3bd1b304d72c0a03f3c6
praveencloudangles <112871831+praveencloudangles@users.noreply.github.com>
2024-03-20 17:47:24 +0530

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,15 +1,29 @@
 print("feature engineering.........")
-from data_cleaning import data_cleaning
 import pandas as pd
+import numpy as np
 
-def feat_eng():
-    final_df = data_cleaning()
-    print(final_df)
-    
-    df = final_df.iloc[:50000]
-    
-    df.to_csv("Retail_suggetion.csv", index=False)
-    
-    return final_df
-    
-feat_eng()
+
+def add_true_randomly(row):
+
+    # Choose the number of columns to add True values randomly
+    num_columns = np.random.randint(3, 110)  # Choose randomly between 2 and 3 columns
+
+    # Randomly select num_columns columns from the DataFrame
+    chosen_columns = np.random.choice(df.columns, size=num_columns, replace=False)
+
+    # Add two or more True values to the chosen columns in the current row
+    for column in chosen_columns:
+        row[column] = True
+
+    return row
+
+path = "https://stock-market-usecase.s3.amazonaws.com/Retail_suggetion.csv"
+df = pd.read_csv(path)
+# Apply the function to each row of the DataFrame
+df_modified = df.apply(add_true_randomly, axis=1)
+
+print(df_modified)
+df_modified.drop(columns=df_modified.columns[0], axis=1, inplace=True)
+print(df_modified)
+
+df_modified.to_csv("final.csv", index=False)
